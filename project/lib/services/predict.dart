8dc650c4d5c70c0f547cc6756cd9948811a4fceb
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2025-04-19 18:05:42 -0300

    bug: always predic person

--- project/lib/services/predict.dart
+++ project/lib/services/predict.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:image/image.dart' as img;
+
 import '../core/constants.dart';
 import '../schemas/result.dart';
 
@@ -15,93 +17,76 @@ class PredictService {
   }
 
   Uint8List imageToByteListFloat32(
-    Uint8List imageBytes,
+    Uint8List imagePixels,
     int height,
     int width,
   ) {
-
-    var convertedBytes = Float32List(1 * height * width * 3);
-    List<double> buffer = List.filled(
-      (imageBytes.length ~/ 4) * 3,
-      0.0,
-    ); 
+    final convertedBytes = Float32List(1 * height * width * 3);
     int pixelIndex = 0;
 
-    for (int i = 0; i <= imageBytes.length - 4; i += 4) {
-      var r = imageBytes[i] / 255.0;
-      var g = imageBytes[i + 1] / 255.0;
-      var b = imageBytes[i + 2] / 255.0;
+    for (int i = 0; i <= imagePixels.length - 4; i += 4) {
+      var r = imagePixels[i] / 255.0;
+      var g = imagePixels[i + 1] / 255.0;
+      var b = imagePixels[i + 2] / 255.0;
 
-      buffer[pixelIndex++] = r;
-      buffer[pixelIndex++] = g;
-      buffer[pixelIndex++] = b;
+      convertedBytes[pixelIndex++] = r;
+      convertedBytes[pixelIndex++] = g;
+      convertedBytes[pixelIndex++] = b;
     }
-
 
     return convertedBytes.buffer.asUint8List();
   }
 
   Uint8List preProcessament(File file) {
     debugPrint("tentando processar a imagem");
-    var imageBytes = file.readAsBytesSync();
-    debugPrint("image processada");
+    final originalImage = img.decodeImage(file.readAsBytesSync())!;
+    final resizedImage = img.copyResize(originalImage, width: 640, height: 640);
+    final imageBytes = resizedImage.getBytes();
 
     final result = imageToByteListFloat32(imageBytes, 640, 640);
-    debugPrint("Resultado do pré-processamento: ${result.toString()}");
+    debugPrint("Resultado do pré-processamento: ${result.length}");
 
     return result;
   }
 
   PredictionResult processOutput(List<List<List<double>>> output) {
-      // Exemplo: Pegue o índice com maior probabilidade
-      // Ajuste esta lógica com base no que o modelo retorna
-      double maxScore = 0;
-      int bestClassIndex = -1;
+    double maxScore = 0;
+    int bestClassIndex = -1;
 
-      for (int i = 0; i < output[0].length; i++) {
-        for (int j = 0; j < output[0][i].length; j++) {
-          if (output[0][i][j] > maxScore) {
-            maxScore = output[0][i][j];
-            bestClassIndex = i; // Ou ajuste conforme necessário
-          }
+    for (int i = 0; i < output[0].length; i++) {
+      for (int j = 0; j < output[0][i].length; j++) {
+        if (output[0][i][j] > maxScore) {
+          maxScore = output[0][i][j];
+          bestClassIndex = i;
         }
       }
-
-      debugPrint(
-        "Classe com maior probabilidade: $bestClassIndex, Pontuação: $maxScore",
-      );
-      return PredictionResult(classIndex: bestClassIndex, score: maxScore);
     }
+
+    debugPrint(
+      "Classe com maior probabilidade: $bestClassIndex, Pontuação: $maxScore",
+    );
+    return PredictionResult(classIndex: bestClassIndex, score: maxScore);
+  }
 
   Future<PredictionResult> runModel(Uint8List imageTensor) async {
     debugPrint("tentando rodar o modelo");
-    var inputShape = interpreter.getInputTensor(0).shape;
+    final inputShape = interpreter.getInputTensor(0).shape;
     debugPrint("Input shape esperado: $inputShape");
 
-    var outputShape = interpreter.getOutputTensor(0).shape;
+    final outputShape = interpreter.getOutputTensor(0).shape;
     debugPrint("Output shape esperado: $outputShape");
 
-    try {
-      // final output = List<double>.filled(2, 0).reshape([1, 2]); // em caso de classificação binaria
-      // Crie o buffer de saída com a forma correta
-      final output = List<double>.filled(
-        outputShape.reduce(
-          (a, b) => a * b,
-        ), // Calcula o tamanho total do buffer
-        0,
-      ).reshape(outputShape);
-      // debugPrint("parte do modelo rodou wiii");
-      interpreter.run(imageTensor, output);
-      // debugPrint("modelo rodou wiii");
+    final input = imageTensor.buffer.asFloat32List().reshape(inputShape);
+    final output = List<double>.filled(
+      outputShape.reduce((a, b) => a * b),
+      0,
+    ).reshape(outputShape);
 
+    try {
+      interpreter.run(input, output);
       debugPrint("output: $output");
 
-      // Processar o resultado (exemplo: pegar o índice com maior probabilidade)
-      final result = processOutput(output.cast<List<List<double>>>());
-
-      // final result = output[0][0] > 0.5 ? 1 : 0; // use em caso de classificação binária
-
-      return result;
+      return processOutput(output.cast<List<List<double>>>());
     } catch (e) {
       debugPrint("Erro ao rodar o modelo: $e");
       throw Exception("Erro ao rodar o modelo");
@@ -120,7 +105,6 @@ class PredictService {
     debugPrint("Input Processado");
 
     final result = await runModel(imageTensor);
-
     debugPrint("resultado do predict: $result");
 
     return result;
